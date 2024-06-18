0f8decbb9e40b085220ba907fa2a3dea0a04903a
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2024-06-18 13:27:42 -0300

    Entidade, persistência e endpoint do Flight e Hotel

---
+++ mono/travel-app/src/main/java/Hotel/Hotel.java
@@ -0,0 +1,11 @@
+package Hotel;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class Hotel extends PanacheEntity {
+
+    public Long travelOrderId;
+    public Integer nights;
+}
