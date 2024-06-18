792729a4420fd68e068907358197987942a9f4b8
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2024-06-18 00:55:21 -0300

    Entidade, persistência e endpoint TravelOrder

---
+++ mono/travel-app/src/main/java/com/alxndrestvs/travelorder/TravelOrder.java
@@ -0,0 +1,11 @@
+package com.alxndrestvs.travelorder;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Entity;
+
+@Entity
+public class TravelOrder extends PanacheEntity {
+
+
+
+}
