a30b370c3c8248796ea0635accef33cccb5a9c91
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2024-06-20 20:08:28 -0300

    Commit inicial do hotel-app (migração do monólito para microserviço)

---
+++ micro/hotel-app/src/main/java/com/alxndrestvs/hotel/HotelResource.java
@@ -0,0 +1,40 @@
+package com.alxndrestvs.hotel;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("hotel")
+public class HotelResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Hotel> hotels() {
+        return Hotel.listAll();
+    }
+
+    @GET
+    @Path("findById")
+    public Hotel findById(@QueryParam("id") long id) {
+        return Hotel.findById(id);
+    }
+
+    @GET
+    @Path("findByTravelOrderId")
+    public Hotel findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId) {
+        return Hotel.findByTravelOrderId(travelOrderId);
+    }
+
+    @Transactional
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hotel newHotel(Hotel hotel) {
+        hotel.id = null;
+        hotel.persist();
+
+        return hotel;
+    }
+}
