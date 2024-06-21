8e0de35031de77c3f5b145931a76dfb95bd91390
Alexandre Esteves <alexandreestevesfclaro@gmail.com>
2024-06-20 22:10:25 -0300

    Commit inicial do travelorder-app (migração do monólito para microserviço)

---
+++ micro/travelorder-app/src/main/java/com/alxndrestvs/travelorder/FlightService.java
@@ -0,0 +1,29 @@
+package com.alxndrestvs.travelorder;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@RegisterRestClient(baseUri = "https://localhost:8081/flight")
+public interface FlightService {
+
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<Flight> flights() {
+//        return Flight.listAll();
+//    }
+
+    @GET
+    @Path("findById")
+    public Flight findById(@QueryParam("id") long id);
+
+    @GET
+    @Path("findByTravelOrderId")
+    public Flight findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Flight newFlight(Flight flight);
+}
---
+++ micro/travelorder-app/src/main/java/com/alxndrestvs/travelorder/HotelService.java
@@ -0,0 +1,29 @@
+package com.alxndrestvs.travelorder;
+
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.util.List;
+
+@RegisterRestClient(baseUri = "https://localhost:8082/hotel")
+public interface HotelService {
+
+//    @GET
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public List<Hotel> hotels();
+
+    @GET
+    @Path("findById")
+    public Hotel findById(@QueryParam("id") long id);
+
+    @GET
+    @Path("findByTravelOrderId")
+    public Hotel findByTravelOrderId(@QueryParam("travelOrderId") long travelOrderId);
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Hotel newHotel(Hotel hotel);
+}
